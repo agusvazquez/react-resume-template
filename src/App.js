dce2a6b6f6fc702105cdcbd8c6720495f9e50b57
Agustin Alejandro Vazquez Ballan <aguss.vazquez@gmail.com>
2021-03-26 14:13:26 -0300

    Added information for Contact, and education. Linked my CV

--- src/App.js
+++ src/App.js
@@ -52,7 +52,9 @@ class App extends Component {
         <Portfolio data={this.state.resumeData.portfolio}/>
         <Testimonials data={this.state.resumeData.testimonials}/>
         <Contact data={this.state.resumeData.main}/>
+
         <Footer data={this.state.resumeData.main}/>
+        
       </div>
     );
   }
