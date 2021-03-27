47313b5e5f03b3c9ef1481aa794936c95b4d7315
Agustin Alejandro Vazquez Ballan <aguss.vazquez@gmail.com>
2021-03-27 18:38:20 -0300

    Portfolio section

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,8 +8,8 @@ class Portfolio extends Component {
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
+            <a href={projects.url} target="_blank"  title={projects.title}>
+               <img class="rounded" alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
                  <h5>{projects.title}</h5>
